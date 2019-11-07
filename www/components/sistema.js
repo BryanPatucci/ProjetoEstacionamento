55d7586d1dd18db84c5dd83eae9f1245564644bf
BryanPatucci <bryanpatuccisilva2002@gmail.com>
2019-11-07 13:18:36 +0900

    Arrumados alguns erros apresentados no commit anterior como erros de portugues, alguns erros o layout e entre outros.Adicionamos mais funções ao js e php para que o banco funcione corretamente\(não esta pronto ainda\).

--- www/components/sistema.js
+++ www/components/sistema.js
@@ -4,7 +4,7 @@ var parametros = {
   "horaE":$("#horaE").val(),
   "nomeProp":$("#nomeProp").val(),
   "modelo":$("#modelo").val(),
-  "tipo":$("#tipo").val(),
+  "tipo":$("option:selected", ("#tipo")).text(),
   "marca":$("#marca").val(),
   "placa":$("#placa").val()
 };
@@ -16,10 +16,10 @@ data:parametros,
 success: function(data){
   navigator.notification.alert(data);
   $("horaE").val("");
-  $("nomeProp").val("")
-  $("modelo").val("")
-  $("tipo").val("")
-  $("marca").val("")
+  $("nomeProp").val("");
+  $("modelo").val("");
+  $("tipo").val("");
+  $("marca").val("");
   $("placa").val("")
 },
 
@@ -28,4 +28,48 @@ error: function(data){
     }
   });
 });
- 
+
+/*listar*/
+$(document).on("click","#busca",function(){
+    var parametro ={
+      "placa":$("#placa").val()
+    };
+
+    $.ajax({
+      type: "post",
+      url:"https://.000webhostapp.com/listar.php",
+      data:parametro,
+      dataType:"json",
+
+      success: function(data){
+        $("#horaE").val(data.entrada.horaE);
+        $("#nomeProp").val(data.entrada.nomeProp);
+        $("#horaS").val(data.saida.horaS);
+        $("#modelo").val(data.entrada.modelo);
+        $("#marca").val(data.entrada.marca);
+        $("#tipo").val(data.entrada.tipo)
+      },
+
+      error: function(data){
+        navigator.notification.alert("Erro ao listar carros cadastrados!");
+      }
+  });
+});
+
+
+/*Mascara input*/
+$(document).ready(function(){
+      $('#placa').mask('AAA-9999')
+      });
+
+/*Voltar*/
+$(document).on("click","#voltar", function(){
+$(location).attr
+(history.go(-1));
+});
+
+/*select*/
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+        
